b2f3e99280d310d275ef6ae26d6546b92b8c319b
vosjedev <hetlachendevosje@gmail.com>
2023-07-16 08:33:43 +0200

    decided to not use a client/server model for now

--- scripts/client.py
+++ scripts/client.py
@@ -1,12 +1,14 @@
 """
 The idea of this file is the following:
- - listen to keyserver.py's socket
- - look if the file corresponding to that key exists
- - use playsound in a thread to start playing the sound.
- 
-Every client has a different instrument. The reason for the server and client being seperated processes/threads has the advantage of being able
-to play multiple instruments/filesgroeps at the same time by connecting multiple clients to one keyserver.
+This file currently does nothing, but see ideas.md#1. A client/server model: for more info about the
+client/server idea.
+
+
+
+
 """
+
+
 
 
 
