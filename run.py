226637a397b1c8295fa2fb705d7265e6641ebd3a
vosjedev <hetlachendevosje@gmail.com>
2023-10-17 09:41:44 +0200

    started a runner

---
+++ run.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""
+This file should first let the user choose an instument/filegroep, then start the keyserver, and then start a client for every instument
+selected.
+"""
+
+
+
+
+print("Not done yet. Use sound-board.py for now.")
+exit()
+
+print("importing scripts...")
+from scripts import keyserver,client
+
